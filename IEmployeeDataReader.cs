812ccb2ab3250fc5a56f14cd325995e542704771
Keerthick ragul R <keerthickragul139@gmail.com>
2025-11-25 17:18:49 +0530

    Add files via upload

---
+++ IEmployeeDataReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollProject
+{
+    public interface IEmployeeDataReader
+    {
+        // Fixed spelling: GetEmployeeRecord
+        EmployeeRecord GetEmployeeRecord(int employeeId);
+    }
+}
---
+++ MockEmployee.cs
@@ -0,0 +1,52 @@
+﻿
+namespace PayrollProject
+{
+    public class MockEmployeeDataReader : IEmployeeDataReader
+    {
+        public EmployeeRecord GetEmployeeRecord(int employeeId)
+        {
+            // Return hardcoded data consistent with IDs 101, 102, 103
+            if (employeeId == 101)
+            {
+                return new EmployeeRecord
+                {
+                    Id = 101,
+                    Name = "Logesh",
+                    Role = "Developer",
+                    IsVetern = false
+                };
+            }
+
+            if (employeeId == 102)
+            {
+                return new EmployeeRecord
+                {
+                    Id = 102,
+                    Name = "Prathees",
+                    Role = "Manager",
+                    IsVetern = true
+                };
+            }
+
+            if (employeeId == 103)
+            {
+                return new EmployeeRecord
+                {
+                    Id = 103,
+                    Name = "Waseef",
+                    Role = "Intern",
+                    IsVetern = false
+                };
+            }
+
+            // Default/unknown
+            return new EmployeeRecord
+            {
+                Id = employeeId,
+                Name = "Unknown",
+                Role = "Unknown",
+                IsVetern = false
+            };
+        }
+    }
+}
